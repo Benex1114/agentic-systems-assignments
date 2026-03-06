f2982e9ea411d636c2961c337d7e4f1def4db89d
Benex1114 <benex1114@gmail.com>
2026-03-06 21:42:06 +0530

    FastAPI basic code

---
+++ fastapi-fundamentals/main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/search")
+def search(name: str, age: int):
+    return {"name" : name, "age" : age}
